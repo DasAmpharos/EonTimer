f440431ca0474689406129a48a1c537aa3f106a5
Dylan Meadows <dylmeadows@gmail.com>
2019-05-02 20:50:44 -0400

    Fixing settings dialog reopening at reduced size

--- src/main/kotlin/io/github/dylmeadows/eontimer/controller/settings/SettingsDialog.kt
+++ src/main/kotlin/io/github/dylmeadows/eontimer/controller/settings/SettingsDialog.kt
@@ -4,6 +4,7 @@ import io.github.dylmeadows.eontimer.config.AppProperties
 import io.github.dylmeadows.eontimer.model.resource.FxmlResource
 import io.github.dylmeadows.springboot.javafx.fxml.SpringFxmlLoader
 import javafx.application.Platform
+import javafx.scene.Parent
 import javafx.scene.control.ButtonType
 import javafx.scene.control.Dialog
 import org.springframework.beans.factory.annotation.Autowired
@@ -14,18 +15,13 @@ class SettingsDialog @Autowired constructor(
     private val properties: AppProperties,
     loader: SpringFxmlLoader) {
 
-    private lateinit var dialog: Dialog<Unit>
-
-    init {
-        Platform.runLater {
-            dialog = Dialog()
-            dialog.title = properties.fullApplicationName
-            dialog.dialogPane.content = loader.load(FxmlResource.SettingsControlPane.get())
-            dialog.dialogPane.buttonTypes.setAll(ButtonType.OK)
-        }
-    }
+    private val settingsControlPane = loader.load<Parent>(FxmlResource.SettingsControlPane.get())
 
     fun showAndWait() {
+        val dialog = Dialog<Void>()
+        dialog.title = properties.fullApplicationName
+        dialog.dialogPane.content = settingsControlPane
+        dialog.dialogPane.buttonTypes.setAll(ButtonType.OK)
         dialog.showAndWait()
     }
 }
