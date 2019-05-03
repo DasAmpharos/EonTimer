b4544f63398093eb8a3efa797b6b4e7354f58a15
dylmeadows <dylmeadows@gmail.com>
2019-05-03 11:12:35 -0400

    DSI FPS is set to 59.9261 instead of 59.8261 (#24)

--- src/main/kotlin/io/github/dylmeadows/eontimer/model/settings/Console.kt
+++ src/main/kotlin/io/github/dylmeadows/eontimer/model/settings/Console.kt
@@ -5,7 +5,7 @@ import io.github.dylmeadows.common.javafx.util.Choice
 enum class Console(private val text: String, val fps: Double) : Choice {
     GBA("GBA", 59.7271),
     NDS("NDS", 59.8261),
-    DSI("DSI", 59.9261),
+    DSI("DSI", 59.8261),
     _3DS("3DS", 59.8261);
 
     val frameRate: Double get() = 1000 / fps
