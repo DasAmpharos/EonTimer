d9353b527f66dce0b7358c8e5510ba0af66c786c
J K <jonathankurtz@protonmail.com>
2021-10-17 20:28:53 -0500

    Application now works again

--- make/CMakeFiles/EonTimer.dir/EonTimer_autogen/3YJK5W5UP7/qrc_resources.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/EonTimer.dir/EonTimer_autogen/3YJK5W5UP7/qrc_resources.cpp.o: \
- /home/helix/Programming/C++/EonTimer/make/EonTimer_autogen/3YJK5W5UP7/qrc_resources.cpp \
- /usr/include/stdc-predef.h
