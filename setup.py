23fbfa154141cd1e3c64e8304cbbca8c7073404b
Dylan Meadows <dylan_r_meadows@homedepot.com>
2024-04-14 16:59:03 -0400

    WIP

--- setup.py
+++ setup.py
@@ -1,30 +1,17 @@
 #!/usr/bin/env python3
 
-import os
 from setuptools import find_packages, setup
 
-name = 'eon-timer'
-package_name = name.replace('-', '_')
-packages = find_packages(package_name)
-packages = list(map(lambda it: f'{package_name}.{it}', packages))
-
-
-def get_package_data() -> dict[str, list[str]]:
-    resources = []
-    pkg_path = os.path.join(package_name, 'resources')
-    for dirname, _, files in os.walk(pkg_path):
-        if '__pycache__' not in dirname:
-            files = filter(lambda it: not it.endswith('.py'), files)
-            file_paths = list(map(lambda it: os.path.join(dirname, it), files))
-            resources.extend(file_paths)
-    pkg_name = pkg_path.replace(os.path.pathsep, '.')
-    return {pkg_name: resources}
-
-
 setup(
-    name=name,
+    name='eon-timer',
     version='3.0.0',
-    packages=packages,
+    packages=list(map(lambda it: f'eon_timer.{it}', find_packages('eon_timer'))),
     include_package_data=True,
-    package_data=get_package_data()
+    package_data={
+        'eon_timer/resources/fonts': ['*.ttf'],
+        'eon_timer/resources/icons': ['*.png'],
+        'eon_timer/resources/images': ['*.png'],
+        'eon_timer/resources/sounds': ['*.wav'],
+        'eon_timer/resources/styles': ['*.scss'],
+    }
 )
