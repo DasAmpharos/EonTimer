45ee2e6f13fbfb010fae649eec1b8513bb59b4cc
Dylan Meadows <dylan_r_meadows@homedepot.com>
2024-08-07 23:53:31 -0400

    Code cleanup

--- eon_timer/util/pyside/numeric_input_field.py
+++ eon_timer/util/pyside/numeric_input_field.py
@@ -1,12 +1,13 @@
 from abc import abstractmethod
 from enum import IntEnum, auto
 from numbers import Number
-from typing import Callable, Final, Generic, TypeVar, override
+from typing import Callable, Final, Generic, TypeVar, final, override
 
 from PySide6.QtCore import QObject
 from PySide6.QtGui import QDoubleValidator, QKeyEvent, QValidator, Qt
 from PySide6.QtWidgets import QLineEdit
 
+from eon_timer.util import strings
 from eon_timer.util.properties.property import FloatProperty, IntProperty, Property
 from eon_timer.util.properties.property_change import PropertyChangeEvent
 
@@ -74,35 +75,18 @@ class NumericInputField(QLineEdit, Generic[NumericT]):
         self.blank_behavior = BlankBehavior.PLACEHOLDER
 
     @abstractmethod
-    def _to_str(self, value: NumericT | None) -> str:
+    def to_number(self, s: str | None) -> NumericT | None:
         ...
 
     @abstractmethod
-    def _to_value(self, text: str | None) -> NumericT | None:
+    def to_string(self, n: NumericT | None) -> str:
         ...
 
     @abstractmethod
-    def set_range(self, bottom: NumericT, top: NumericT) -> None:
+    def set_range(self, lower_bound: NumericT, upper_bound: NumericT) -> None:
         ...
 
-    def get_value(self) -> NumericT:
-        return self.value.get()
-
-    def set_value(self, new_value: NumericT) -> None:
-        self.value.set(new_value)
-
-    def __on_property_changed(self, event: PropertyChangeEvent[NumericT]) -> None:
-        self.setText(self._to_str(event.new_value))
-
-    def __on_text_changed(self, text: str) -> None:
-        value = self._to_value(text)
-        if value is None and self.blank_behavior == BlankBehavior.PLACEHOLDER:
-            self.setPlaceholderText(str(self.value.get()))
-            return
-        if value is not None and value != self.get_value():
-            self.setPlaceholderText('')
-            self.value.set(value)
-
+    @final
     @override
     def keyPressEvent(self, event: QKeyEvent):
         super().keyPressEvent(event)
@@ -112,16 +96,52 @@ class NumericInputField(QLineEdit, Generic[NumericT]):
             case Qt.Key.Key_Down:
                 self.value.set(self.value.get() - 1)
 
+    def get_value(self) -> NumericT:
+        return self.value.get()
+
+    def set_value(self, value: NumericT) -> None:
+        self.value.set(value)
+
+    def __on_property_changed(self, event: PropertyChangeEvent[NumericT]) -> None:
+        self.setText(self.to_string(event.new_value))
+
+    def __on_text_changed(self, text: str) -> None:
+        value = self.to_number(text)
+        if value is None and self.blank_behavior == BlankBehavior.PLACEHOLDER:
+            self.setPlaceholderText(str(self.value.get()))
+            return
+        if value is not None and value != self.get_value():
+            self.setPlaceholderText('')
+            self.value.set(value)
+
 
 class IntInputField(NumericInputField[int]):
     def __init__(self, value: int = 0, parent: QObject | None = None):
         NumericInputField.__init__(self, IntProperty(value), parent)
-        self.validator: Final = RadixValidator(self.to_int)
+        self.validator: Final = RadixValidator(lambda s, radix: int(s, radix))
         super().setValidator(self.validator)
 
-    @staticmethod
-    def to_int(text: str, radix: Radix) -> int:
-        return int(text, radix)
+    @override
+    def to_number(self, s: str | None) -> int | None:
+        s = strings.strip_to_none(s)
+        if s is None:
+            return None
+        return int(s, self.validator.radix)
+
+    @override
+    def to_string(self, n: int | None) -> str:
+        if n is None:
+            return ''
+
+        match self.validator.radix:
+            case Radix.BINARY:
+                return '{:b}'.format(n)
+            case Radix.OCTAL:
+                return '{:o}'.format(n)
+            case Radix.HEXADECIMAL:
+                return '{:X}'.format(n)
+            case Radix.DECIMAL:
+                return str(n)
 
     @override
     def set_range(self, lower_bound: int, upper_bound: int) -> None:
@@ -130,62 +150,63 @@ class IntInputField(NumericInputField[int]):
         self.value.set(value)
 
     @override
-    def set_value(self, new_value: int) -> None:
-        if self.validator.lower_bound > new_value:
+    def set_value(self, value: int) -> None:
+        if self.validator.lower_bound > value:
             raise ValueError(f'new_value must be greater than or equal to {self.validator.lower_bound}')
-        if self.validator.upper_bound < new_value:
+        if self.validator.upper_bound < value:
             raise ValueError(f'new_value must be less than or equal to {self.validator.upper_bound}')
-        super().set_value(new_value)
+        super().set_value(value)
 
-    @override
-    def _to_str(self, value: int | None) -> str:
-        if value is None:
-            return ''
+    @property
+    def radix(self) -> Radix:
+        return self.validator.radix
 
-        match self.validator.radix:
-            case Radix.BINARY:
-                return '{:b}'.format(value)
-            case Radix.OCTAL:
-                return '{:o}'.format(value)
-            case Radix.DECIMAL:
-                return str(value)
-            case Radix.HEXADECIMAL:
-                return '{:X}'.format(value)
-
-    @override
-    def _to_value(self, text: str | None) -> int | None:
-        return None if not text else self.to_int(text, self.validator.radix)
-
-    def set_radix(self, radix: Radix) -> None:
-        self.validator.radix = radix
-        new_text = self._to_str(self.value.get())
+    @radix.setter
+    def radix(self, value: Radix) -> None:
+        self.validator.radix = value
+        new_text = self.to_string(self.value.get())
         self.setText(new_text)
 
 
 class FloatInputField(NumericInputField[float]):
     def __init__(self, value: float = 0.0, parent: QObject | None = None):
         NumericInputField.__init__(self, FloatProperty(value), parent)
-        self.__validator = QDoubleValidator()
-        super().setValidator(self.__validator)
+        self.validator: Final = QDoubleValidator()
+        super().setValidator(self.validator)
+        self.precision = 2
 
     @override
-    def set_range(self, bottom: float, top: float) -> None:
-        self.__validator.setRange(bottom, top)
-        value = min(max(self.value.get(), bottom), top)
+    def to_number(self, s: str | None) -> float | None:
+        if s is None:
+            return None
+        return float(s)
+
+    @override
+    def to_string(self, n: float | None) -> str:
+        if n is None:
+            return ''
+        return f'{n:.{self.precision}f}'
+
+    @override
+    def set_range(self, lower_bound: float, upper_bound: float) -> None:
+        self.validator.setRange(lower_bound, upper_bound)
+        value = min(max(self.value.get(), lower_bound), upper_bound)
         self.value.set(value)
 
     @override
-    def set_value(self, new_value: float) -> None:
-        if self.__validator.bottom() > new_value:
-            raise ValueError(f'new_value must be greater than or equal to {self.__validator.bottom()}')
-        if self.__validator.top() < new_value:
-            raise ValueError(f'new_value must be less than or equal to {self.__validator.top()}')
-        super().set_value(new_value)
+    def set_value(self, value: float) -> None:
+        if self.validator.bottom() > value:
+            raise ValueError(f'new_value must be greater than or equal to {self.validator.bottom()}')
+        if self.validator.top() < value:
+            raise ValueError(f'new_value must be less than or equal to {self.validator.top()}')
+        super().set_value(value)
 
-    @override
-    def _to_str(self, value: float | None) -> str:
-        return '' if value is None else '{:.2f}'.format(value)
+    @property
+    def precision(self) -> int:
+        return self.__precision
 
-    @override
-    def _to_value(self, text: str | None) -> float | None:
-        return None if not text else float(text)
+    @precision.setter
+    def precision(self, value: int) -> None:
+        self.__precision = value
+        new_text = self.to_string(self.value.get())
+        self.setText(new_text)
--- eon_timer/util/strings.py
+++ eon_timer/util/strings.py
@@ -1,3 +1,5 @@
-def strip_to_none(s: str) -> str | None:
+def strip_to_none(s: str | None) -> str | None:
+    if s is None:
+        return None
     stripped = s.strip()
     return stripped if stripped else None
